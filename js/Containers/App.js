c125801475ac7dc30a6dd31461187f7e62832941
Daniel Luca <lucadanielcostin@gmail.com>
2016-03-16 19:01:08 +0200

    Reindent code

--- js/Containers/App.js
+++ js/Containers/App.js
@@ -1,67 +1,55 @@
-import React, { Component, PropTypes } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import React, {Component, PropTypes} from 'react'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
 import * as Actions from '../Actions'
 
 class App extends Component {
-    componentDidMount () {
-        const { store } = this.context
+    componentDidMount() {
+        const {store} = this.context
         this.unsubscribe = store.subscribe(() => {
             this.forceUpdate()
         })
     }
 
-    componentWillUmount () {
+    componentWillUmount() {
         this.unsubscribe()
     }
 
-    render () {
-        const { store } = this.context
+    render() {
+        const {store} = this.context
         const state = store.getState()
-
-        console.log(state)
 
         return (
             <div>
-                <input
-                    type="text"
-                    ref="name"
-                    value={state.input.value}
-                    onKeyPress={(e) => {
-                        console.log(e.charCode)
-                        if (e.charCode === 13) {
-                            store.dispatch(Actions.addTodo(e.target.value))
-                            store.dispatch(Actions.emptyInput())
-                        }
-                    }}
-                    onChange={(e) => {
-                        store.dispatch(Actions.updateInput(e.target.value))
-                    }}
-                    />
-                <button
-                    onClick={(e) => {
-                        store.dispatch(Actions.addTodo(this.refs.name.value))
-                        store.dispatch(Actions.emptyInput())
-                    }}>
+                <input type="text" ref="name" value={state.input.value} onKeyPress={(e) => {
+                    if (e.charCode === 13) {
+                        store.dispatch(Actions.addTodo(e.target.value));
+                        store.dispatch(Actions.emptyInput());
+                    }
+                }} onChange={(e) => {
+                    store.dispatch(Actions.updateInput(e.target.value));
+                }} placeholder="Input something"
+                />
+                <button onClick={(e) => {
+                    store.dispatch(Actions.addTodo(this.refs.name.value));
+                    store.dispatch(Actions.emptyInput());
+                }}>
                     Add todo
                 </button>
 
                 <ol>
-                    {state.todos.map(
-                        (todo) => {
-                            return (
-                                <li
-                                    key={todo.id}
-                                    onClick={(e) => {
-                                        store.dispatch(Actions.toggleTodo(todo.id))
-                                    }}
-                                >
-                                    {todo.text} -
-                                    {todo.completed === true ? 'yes' : 'no'}
-                                </li>
-                            )
-                        }
-                    )}
+                    {state.todos.map((todo) => {
+                        return (
+                            <li key={todo.id} onClick={(e) => {
+                                store.dispatch(Actions.toggleTodo(todo.id))
+                            }}>
+                                {todo.text}
+                                - {todo.completed === true
+                                    ? 'yes'
+                                    : 'no'}
+                            </li>
+                        )
+                    })}
                 </ol>
             </div>
         )
